3e1d0f5e0ccc41694549d9d53f6be2d4eaaec934
TangJF <18681268108@163.com>
2020-10-28 14:28:06 +0800

    todolsit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import Menu from './components/Menu'
+import {menuList} from './mock/menu'
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            activePath: []
+        }
+    }
+    render() {
+        return (
+            <Menu
+                list={menuList}
+                dep={0}
+                activePath={this.state.activePath}
+                changeActivePath={this.changeActivePath.bind(this)}
+            />
+        )
+    }
+    changeActivePath(dep, id, list) {
+        let activePath = this.state.activePath
+        if (!activePath[dep] || dep == activePath.length - 1) {
+            //最后一个 添进去即可
+            activePath[dep] = id
+        } else {
+            //重新构建整个activePath数组
+            activePath[dep] = id
+            let cur = []
+            for (let i = 0; i < list.length; i++) {
+                let itemId = list[i].id
+                if (itemId == id) {
+                    cur = list[i]
+                    break
+                }
+            }
+            setPath(dep + 1, cur)
+        }
+        function setPath(dep, cur) {
+            if (cur.children) {
+                activePath[dep] = cur.children[0].id
+                setPath(dep + 1, cur.children[0])
+            }
+        }
+        this.setState({
+            activePath
+        })
+    }
 }
-
-export default App;
+export default App
